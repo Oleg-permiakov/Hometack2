e74c1cc6d6c80b39644c3421b7f7aa7be4f2c59b
Oleg <schika145@gmail.com>
2023-10-16 16:46:48 +0500

    Конец задачи

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Какое из чисел больше, а какое меньше
+Console.WriteLine("Введите первое число");
+string namber1 = Console.ReadLine();
+int a = Convert.ToInt32(namber1);
+Console.WriteLine("Введите второе число");
+string namber2 = Console.ReadLine();
+int b = Convert.ToInt32(namber2);
+if (a > b)
+{
+    Console.WriteLine(a + " Больше чем " + b);
+}    
+if (b > a)
+{
+    Console.WriteLine(b + " Больше чем " + a);
+}    
